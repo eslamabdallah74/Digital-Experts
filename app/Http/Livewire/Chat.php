af0ac75020b537638f14800ed5cb7e11b392b386
eslamabdallah74 <eslamabdallah301@gmail.com>
2022-09-09 18:27:28 +0200

    chat - error in refresh same chat

---
+++ app/Http/Livewire/Chat.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Message;
+use App\Models\User;
+use Livewire\Component;
+
+class Chat extends Component
+{
+    public $users;
+    public $allMessages = [];
+    public $activeUserChat;
+
+    public $search = '';
+
+    public $message;
+
+
+
+    public function mount()
+    {
+
+        if ($this->activeUserChat)
+        {
+            $this->allMessages  = Message::with('user')
+            ->where(function($query){
+                $query->where('from_user_id',auth()->user()->id)
+                ->where('to_user_id',$this->activeUserChat->id);
+
+            })->orWhere(function($query){
+                $query->where('to_user_id',auth()->user()->id)
+                ->where('from_user_id',$this->activeUserChat->id);
+            })->get();
+        }
+
+    }
+
+    public function getMessage($id)
+    {
+        $this->activeUserChat = User::where('id', $id)->first();
+        $this->mount();
+    }
+
+    protected $rules = [
+        'message' => 'required|min:1',
+    ];
+
+    public function sendMessage()
+    {
+        $validatedData = $this->validate();
+        $sendMessage   = Message::create([
+            'from_user_id'  => auth()->user()->id,
+            'to_user_id'    => $this->activeUserChat->id,
+            'message'       => $this->message,
+        ]);
+        $this->reset('message');
+        $this->mount();
+
+    }
+
+    public function render()
+    {
+        return view('livewire.chat', [
+            $this->users        = User::where('id', '!=', auth()->user()->id)
+            ->where('name','like','%'.$this->search.'%')->orderBy('id','desc')->get()
+        ]);
+    }
+}
